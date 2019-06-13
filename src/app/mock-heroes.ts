a3f94eaaa30d5151ae98bd7330d20716fa4ba9bc
Marc Davies <marcdaviesriot@gmail.com>
2019-06-12 21:41:05 -0700

    add routes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Lux' },
+  { id: 2, name: 'Garen' },
+  { id: 3, name: 'Annie' },
+  { id: 4, name: 'Varus' },
+  { id: 5, name: 'Caitlyn' },
+];
